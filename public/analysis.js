60dca82f41cdb7e517caa21177fb2e7529ed580a
Hashemii2 <hashemi.faezeh2@yahoo.com>
2022-12-13 00:59:52 +0330

    adding analysis

--- public/analysis.js
+++ public/analysis.js
@@ -1,6 +1,23 @@
-export function totalAnalysis(values) {
-  const tchanges = values[1] > 0 ? 'افزایش' : 'کاهش';
-  const wchanges = values[3] > 0 ? 'افزایش' : 'کاهش';
+import { totalAnalysis } from './helper.js';
 
-  return `امروز شاخص کل بورس نسبت به روز گذشته با ${values[1]} میزان ${tchanges} به سطح ${values[0]} واحد رسید. و همچنین شاخص هم وزن با ${values[3]} میزان ${wchanges} به سطح ${values[2]} واحد رسید.`;
+export async function analysis() {
+  const response = await fetch('/api/data');
+  const data = await response.json();
+
+  const values = data.map((obj) => Object.values(obj));
+
+  const inputElem = document.createElement('input');
+  const divElem = document.createElement('div');
+
+  inputElem.type = 'button';
+  inputElem.value = 'تحلیل بازار';
+
+  divElem.setAttribute('id', 'analysis');
+
+  document.body.appendChild(inputElem);
+  document.body.appendChild(divElem);
+
+  inputElem.addEventListener('click', () => {
+    document.querySelector('#analysis').innerHTML = totalAnalysis(values);
+  });
 }

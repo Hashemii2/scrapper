36b97f7ed088e85f8987f35b5764e7d2272352ba
Hashemii2 <hashemi.faezeh2@yahoo.com>
2023-01-02 22:01:15 +0330

    stock scrapper code

--- scrapers/stock.js
+++ scrapers/stock.js
@@ -2,33 +2,38 @@ const axios = require('axios');
 const cheerio = require('cheerio');
 
 async function stockScrapper() {
-  const { data } = await axios.get(
-    'http://www.tsetmc.com/Loader.aspx?ParTree=15'
-  );
-
-  const $ = cheerio.load(data);
   let finalData = [];
 
-  $('#TseTab1Elm > div.box1.white > div.content > table > tbody > tr').each(
-    (_, p) => {
-      let allData = [];
-      let dataObj = {};
+  try {
+    const { data } = await axios.get(
+      'http://www.tsetmc.com/Loader.aspx?ParTree=15'
+    );
 
-      $(p)
-        .find('td')
-        .each((_, c) => {
-          allData.push($(c).text());
-        });
+    const $ = cheerio.load(data);
 
-      dataObj['نماد'] = allData[0];
-      dataObj['قیمت پایانی'] = allData[1];
-      dataObj['آخرین معامله'] = allData[3];
+    $('#TseTab1Elm > div.box1.white > div.content > table > tbody > tr').each(
+      (_, p) => {
+        let allData = [];
+        let dataObj = {};
 
-      finalData.push(dataObj);
-    }
-  );
+        $(p)
+          .find('td')
+          .each((_, c) => {
+            allData.push($(c).text());
+          });
 
+        dataObj['نماد'] = allData[0];
+        dataObj['قیمت پایانی'] = allData[1];
+        dataObj['آخرین معامله'] = allData[3];
+
+        finalData.push(dataObj);
+      }
+    );
+  } catch (err) {
+    console.log(err);
+  }
+  console.log(finalData);
   return finalData;
 }
-
+// stockScrapper();
 module.exports = stockScrapper;
